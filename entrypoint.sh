89f7dda0d39f01bd4b87fd459780550b08aa4620
Rowan Easter-Robinson <rowan@workyourtech.com>
2023-02-06 09:55:22 +0000

    Added input target

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,12 @@ declare -i res
 
 . ${IDF_PATH}/export.sh
 git config --global --add safe.directory /github/workspace
+
+echo "Setting target to $INPUT_IDF_TARGET"
+idf.py set-target $INPUT_IDF_TARGET
+echo "Building..."
 idf.py build
+
 res=$?
 
 echo "::set-output name=result::$res"
